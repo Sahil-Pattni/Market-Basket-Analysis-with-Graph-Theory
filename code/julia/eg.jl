814d1855e354820178ac6b1fb0399821403d49d8
Sahil Pattni <sahilpattni97@gmail.com>
2021-01-01 23:53:02 +0400

    Gave up on Julia for now

--- code/julia/eg.jl
+++ code/julia/eg.jl
@@ -1,7 +1,7 @@
 Threads.nthreads()
 
 # Import packages
-using DataFrames, CSV, JDF, Formatting
+using DataFrames, CSV, JDF, Formatting, Statistics
 
 # Change path
 cd("/Users/sloth_mini/Documents/4.1/dissertation/code/")
@@ -26,12 +26,25 @@ printfmt("There are {2:d} unique baskets and {1:d} products.", size(products, 1)
 # Linear function to generate vector for basket id
 function gen_vector(basket_id)
     transaction = df[df.basket_id .== basket_id, 1]
-    return [product in transaction ? 1 : 0 for product in products]
+    res = Int64[]
+    for i in 1:size(products, 1)
+        push!(res, products[i] in transaction ? 1 : 0)
+    end
+    print(typeof(res))
+    return res
 end
 
-# Linear Approach
+
 seconds_elapsed = @timed begin
-    vectors = [gen_vector(bid) for bid in baskets]
+    # vectorized function
+    vectors = gen_vector.(baskets[1:5])
 end
 
-printfmt("It took {1:.2f} minutes to get the vectors from {2:d} baskets", seconds_elapsed[2]/60, size(baskets, 1))
+vectors = Array(vectors)
+printfmt("It took {1:.2f} minutes to get the vectors from {2:d} baskets (gen, vectorized)", seconds_elapsed[2]/60, size(baskets, 1))
+vectors
+typeof(vectors[1])
+shape(vectors)
+cor(vectors)
+
+cmat[2]
