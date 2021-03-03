bf546597ab4b114fba75ca71dce44ebc57d04980
Sahil Pattni <sahilpattni97@gmail.com>
2021-03-03 08:12:18 +0400

    Added Rust function to generate purchase tuples (failed)

--- code/rust_backend/src/main.rs
+++ code/rust_backend/src/main.rs
@@ -46,14 +46,44 @@ fn make_vectors(path: &str) -> Result<HashMap<u32, [u8; 45]>, Box<dyn Error>> {
     Ok(transactions) // return type with Ok signature
 }
 
+fn write_purchases(in_path: &str, out_path: &str) -> Result<(), Box<dyn Error>>{
+    
+    // Contains Vec<String> of purchases
+    let mut purchases: Vec<Vec<String>> = Vec::new();
+    // CSV Reader
+    let mut reader = Reader::from_path(in_path)?;
+    // CSV Writer
+    let mut writer = Writer::from_path(out_path)?;
+    // Import column names
+    let products = import_unqiue("../data/columns.csv")?; // unique products
+    // Iterate and add products
+    let data = reader.records();
+    for row in data {
+        let mut current_purchase: Vec<String> = Vec::new();
+        let row = row.iter();
+        for (i, item) in row.enumerate() {
+            let item: u8 = item[i].parse()?;
+            if (item as u8) == (1 as u8) {
+                current_purchase.push(products[i].clone());
+            }
+        }
+        purchases.push(current_purchase);
+    }
+
+    // Write to csv
+    for row in purchases {
+        writer.write_record(row)?;
+    }
+
+    Ok(())
+}
+
 fn write_data(path: &str, map: HashMap<u32, [u8; 45]>) -> Result<(), Box<dyn Error>> {
-    // Init writer
     let mut writer = Writer::from_path(path)?;
     // Products for header
     let products = import_unqiue("../data/unique.csv")?; // unique products
-
     writer.write_record(products)?;
-    for (key, val) in map.iter() {
+    for (_, val) in map.iter() {
         let mut temp_vec = Vec::new();
         for v in val {
             temp_vec.push(v.to_string());
@@ -65,17 +95,25 @@ fn write_data(path: &str, map: HashMap<u32, [u8; 45]>) -> Result<(), Box<dyn Err
 
 
 fn main() {
-    let _filepath = "../data/products.csv";
-    let start = Instant::now();
-    let mut num_baskets = 0;
-    match make_vectors(_filepath) {
-        Ok(res) => {
-            num_baskets = res.iter().len();
-            println!("There are {} baskets", num_baskets);
-            write_data("../data/rust_vectors.csv", res);
-        },
+    //let _filepath = "../data/products.csv";
+    //let start = Instant::now();
+    //let mut num_baskets = 0;
+    // match make_vectors(_filepath) {
+    //     Ok(res) => {
+    //         num_baskets = res.iter().len();
+    //         println!("There are {} baskets", num_baskets);
+    //         write_data("../data/rust_vectors_temp.csv", res);
+    //         //write_purchases("../data/purchases.csv", res);
+    //     },
+    //     Err(err) => println!("Error: {:?}", err)
+    // };
+    
+    let _in_path = "../data/rust_vectors.csv";
+    let _out_path = "../data/output/purchases.csv";
+    match write_purchases(_in_path, _out_path) {
+        Ok(()) => {()}
         Err(err) => println!("Error: {:?}", err)
-    };
-    let duration = start.elapsed().as_secs();
-    println!("Finished generating {} baskets in {} seconds", num_baskets, duration);
+    }
+    //let duration = start.elapsed().as_secs();
+    //println!("Finished generating {} baskets in {} seconds", num_baskets, duration);
 }
