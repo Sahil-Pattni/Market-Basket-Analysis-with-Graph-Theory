56edeefb5198f528a46130d645f7bc729e571671
Sahil Pattni <sahilpattni97@gmail.com>
2021-03-03 08:12:42 +0400

    Added Python file to generate purchase tuples

---
+++ code/purchases.py
@@ -0,0 +1,27 @@
+import os
+import csv
+import time
+import pandas as pd
+
+df = pd.read_csv('data/rust_vectors.csv')
+columns = df.columns
+purchases = []
+start = time.time()
+
+limit = 100000
+for i in range(df.shape[0]):
+    current_purchase = []
+    for j in range(df.shape[1]):
+        if df.iloc[i,j] == 1:
+            current_purchase.append(columns[j])
+    purchases.append(current_purchase)
+
+end = time.time() - start
+os.system('say "your code has finished running"')
+print(f'Finished getting purchases in {end/60:,.2f} minutes.')
+estimated_time = (end/limit) * df.shape[0] 
+print(f'Estimated completion time for full dataset is {estimated_time/60:.2f} minutes')
+
+with open('data/purchases.csv', 'w+') as f:
+    writer = csv.writer(f)
+    writer.writerows(purchases)
