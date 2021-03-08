bac394a263249b04b708cd0e1fd01021ea5916db
Sahil Pattni <sahilpattni97@gmail.com>
2021-03-08 14:32:05 +0400

    Made Apriori into a function

--- code/apriori.py
+++ code/apriori.py
@@ -3,21 +3,14 @@ import time
 import pandas as pd
 from efficient_apriori import apriori, itemsets
 
-transactions = []
-with open('data/purchases.csv', 'r') as f:
-    reader = csv.reader(f)
-    #transactions = [tuple(row) for row in reader]
-    i = 0
-    for row in reader:
-        transactions.append(tuple(row))
-        i += 1
-        if i > 2000:
-            break
 
 
-itemset, rules = apriori(transactions, min_support=0.1, min_confidence=0.2)
+def get_apriori_rules(min_support=0.1, min_confidence=0.2):
+    with open('data/purchases.csv', 'r') as f:
+        reader = csv.reader(f)
+        transactions = [tuple(row) for row in reader]
+    _, rules = apriori(transactions, min_support=min_support, min_confidence=min_confidence)
+    # Sort by confidence
+    rules.sort(key=lambda x: x.confidence, reverse=True)
 
-rules.sort(key=lambda x: x.confidence, reverse=True)
-
-for rule in rules:
-    print(f'[{rule.confidence}] {rule}')
+    return rules
