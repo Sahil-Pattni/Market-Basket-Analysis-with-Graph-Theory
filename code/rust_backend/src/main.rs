1253d6a62892cd19a367b96bcd2c3556ccb854a4
Sahil Pattni <sahilpattni97@gmail.com>
2021-04-07 23:27:16 +0400

    Fixed Rust output issue

--- code/rust_backend/src/main.rs
+++ code/rust_backend/src/main.rs
@@ -26,15 +26,11 @@ fn make_vectors(path: &str) -> Result<HashMap<u64, [u8; 45]>, Box<dyn Error>> {
     for result in reader.records() {
         let record = result?; // error check
         let product = record[0].to_string();
-        // let basket_id: u32 = record[4].parse()?;
-        // println!("{}", basket_id);
-        let basket_id: u64 = match record[4].parse() {
-            Ok(id) => id,
-            Err(error) => panic!("Problem with {}: {}", &record[4], error),
-        };
+        let basket_id: u64 = record[4].parse()?;
 
         // Insert basket_id if doesn't exist
         if !transactions.contains_key(&basket_id) {
+            // Insert blank binary purchase vector
             transactions.insert(basket_id, [0; 45]);
         }
     
@@ -48,53 +44,25 @@ fn make_vectors(path: &str) -> Result<HashMap<u64, [u8; 45]>, Box<dyn Error>> {
             }
         }
     }
-    println!("Vectors made!");
     Ok(transactions) // return type with Ok signature
 }
 
-// fn write_purchases(in_path: &str, out_path: &str) -> Result<(), Box<dyn Error>>{
-    
-//     // Contains Vec<String> of purchases
-//     let mut purchases: Vec<Vec<String>> = Vec::new();
-//     // CSV Reader
-//     let mut reader = Reader::from_path(in_path)?;
-//     // CSV Writer
-//     let mut writer = Writer::from_path(out_path)?;
-//     // Import column names
-//     let products = import_unqiue("../data/output/unique_products.csv")?; // unique products
-//     // Iterate and add products
-//     let data = reader.records();
-//     for row in data {
-//         let mut current_purchase: Vec<String> = Vec::new();
-//         let row = row.iter();
-//         for (i, item) in row.enumerate() {
-//             let item: u8 = item[i].parse()?;
-//             if (item as u8) == (1 as u8) {
-//                 current_purchase.push(products[i].clone());
-//             }
-//         }
-//         purchases.push(current_purchase);
-//     }
-
-//     // Write to csv
-//     for row in purchases {
-//         writer.write_record(row)?;
-//     }
-
-//     Ok(())
-// }
 
 fn write_data(path: &str, map: HashMap<u64, [u8; 45]>) -> Result<(), Box<dyn Error>> {
     let mut writer = Writer::from_path(path)?;
     // Products for header
     let products = import_unqiue("../data/output/unique_products.csv")?; // unique products
     writer.write_record(products)?;
+
     for (_, val) in map.iter() {
         let mut temp_vec = Vec::new();
         for v in val {
             temp_vec.push(v.to_string());
         }
-        writer.write_record(temp_vec)?;
+        match writer.write_record(temp_vec) {
+            Ok(emp) => emp,
+            Err(error) => panic!("{}", error)
+        };
     };
     Ok(())
 }
@@ -109,7 +77,6 @@ fn main() {
             num_baskets = res.iter().len();
             println!("There are {} baskets", num_baskets);
             write_data("../data/rust_vectors_temp.csv", res);
-            //write_purchases("../data/purchases.csv", res);
         },
         Err(err) => println!("Error: {:?}", err)
     };
