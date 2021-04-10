cb4ea747eeb70fd3b24c6ad3cbeea711199926b8
Sahil Pattni <sahilpattni97@gmail.com>
2021-04-11 01:55:35 +0400

    Tried some numba stuff

--- code/playground2.py
+++ code/playground2.py
@@ -1,26 +1,5 @@
 x = 0
-for b in range(3):
-    for c in range(3):
-        if b == 2:
-            break
-        x += 1
 
-print(x)
-
-below_threshold = 0
-ab = 0
-below_threshold_count = 0
-
-
-for _ in range(10):
-    # Old
-    if any([set(x).issubset(ab) for x in below_threshold]):
-        below_threshold_count += 1
-        continue
-
-    # New
-    for x in below_threshold:
-        if set(x).issubset(ab):
-            below_threshold_count += 1
-            is_above_threshold = False
-            break
+def upit():
+    global x
+    x += 10
