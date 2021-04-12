08e748408f4d772da230e423bcb07a0ad77038f4
Sahil Pattni <sahilpattni97@gmail.com>
2021-04-12 20:19:37 +0400

    Updated tests for Cython

--- code/playground2.py
+++ code/playground2.py
@@ -4,28 +4,36 @@ import time
 import arm
 from timeit import timeit
 if __name__ == '__main__':
-    csv_filepath = 'data/rust_vectors_product_category.csv'
-    pickle_filepath = 'data/rust_vectors_product_category.pkl'
+    csv_filepath = 'data/rust_vectors_product_category_no_fuel.csv'
+    pickle_filepath = 'data/rust_vectors_product_category_no_fuel.pkl'
+    old_filepath = 'data/rust_vectors.csv'
+
+    min_support = 0.0001
+    min_confidence = 0.25
     
-
-    # start = time.time()
-    # cy.init(csv_filepath, pickled=False, graph_exists=False)
-    # cy.generate_rules()
-    # cy_time = time.time() - start
-
-    start = time.time()
-    cy.init(pickle_filepath, pickled=True, graph_exists=True)
-    rules = cy.generate_rules(min_support=0.005, min_confidence=0.3)
-    cy_optimized_time = time.time() - start
-
     # start = time.time()
     # py = arm.MSTARM(csv_filepath)
-    # py.generate_rules()
+    # rules = py.generate_bicluster_rules(min_support=min_support, min_confidence=min_confidence)
     # py_time = time.time() - start
+    # print(f'Python: {py_time:,.4f} seconds. {len(rules):,} rules.')
 
+    
+    start = time.time()
+    cy.init(csv_filepath, pickled=False, graph_exists=False)
+    rules = cy.generate_bicluster_rules(min_support=min_support, min_confidence=min_confidence)
+    cy_time = time.time() - start
+    print(f'Cython: {cy_time:,.4f} seconds. {len(rules):,} rules. ')
 
-    # print(f'Python: {py_time:,.4f} seconds')
-    # print(f'Cython: {cy_time:,.4f} seconds')
-    print(f'Cython (Optimized): {cy_optimized_time:,.4f} seconds. {len(rules)} rules.')
-    for rule in rules:
-        print(rule)
+    # start = time.time()
+    # cy.init(pickle_filepath, pickled=True, graph_exists=True)
+    # rules = cy.generate_rules(min_support=min_support, min_confidence=min_confidence)
+    # cy_optimized_time = time.time() - start
+    # print(f'Cython (Optimized): {cy_optimized_time:,.4f} seconds. {len(rules):,} rules.')
+
+    if len(rules) > 10:
+        for rule in rules[:10]:
+            print(rule)
+    else:
+        for rule in rules:
+            print(rule)
+    
